e623ff26f75b6874929cd40022ca7529de35721c
Leo <chinobustos5@gmail.com>
2025-03-19 23:18:25 -0300

    se cambio la imagen del carrusel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const carouselImages = [
   {
     id: 1,
-    url: "https://images.unsplash.com/photo-1511746687876-42cb762f6ac1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8amluZXRlcyUyMGNhYmFsbG9zfGVufDB8fDB8fHww",
+    url: "https://images.unsplash.com/photo-1517344296525-3f2079b011d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Inscribir Jinetes",
     description: "Un jinete, un sueño, una meta: la victoria.",
   },
