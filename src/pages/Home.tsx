4371bd0b0850d6f4964088bb6f51385e7de64dd8
Leo <chinobustos5@gmail.com>
2025-03-20 14:46:43 -0300

    cambios sugeridos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const carouselImages = [
   {
     id: 1,
-    url: "../multimedia/jinete.jpg",
+    url: "https://images.unsplash.com/photo-1595676800779-7d98f941e494?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Inscribir Jinetes",
     description: "Un jinete, un sueño, una meta: la victoria.",
   },
   {
     id: 2,
-    url: "https://images.unsplash.com/photo-1617906693778-49ced8bcbb62?q=80&w=1564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1536356953159-9043c2260bba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Inscribir Caballos",
     description: "Donde la destreza y la belleza se encuentran.",
   },
